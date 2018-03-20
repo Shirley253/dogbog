eb33e61cf02ea0732ded606f17fc649fede5e445
andrew lupton and shirley dennis <andrew@>
2018-03-20 14:47:21 -0700

    add toggleClass(es) and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   $("button#black").click(function() {
-    $("body").removeClass();
-    $("body").addClass("black-background");
+    $("body").toggleClass("black-background");
   });
-  $("button#white").click(function() {
-    $("body").removeClass();
-    $("body").addClass("white-background");
+  $("button#border").click(function() {
+    $("li").toggleClass("border-highlight");
   });
 });
